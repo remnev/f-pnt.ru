76324ff0ce423f6c4709c06a5e307cbd0cacc101
Yuriy Remnev <yura_remnev@mail.ru>
2015-01-12 17:53:05 +0300

    Add unit test ability and code coverage

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var jscs = require('gulp-jscs');
+var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
 
 var paths = {
     rebuild: [],
@@ -15,11 +17,22 @@ var paths = {
         // 'middleware/**/*.js',
         'models/**/*.js',
         'updates/*.js',
-        // 'test/**/*.js',
+        'test/**/*.js',
+        '*.js'
+    ],
+    test: [
+        'test/*.js'
+    ],
+    coverage: [
+        'controllers/**/*.js',
+        'lib/**/*.js',
+        // 'middleware/**/*.js',
+        'models/**/*.js',
         '*.js'
     ]
 };
 
+// Lint
 gulp.task('jscs', function () {
     return gulp.src(paths.lint)
         .pipe(jscs());
@@ -33,3 +46,23 @@ gulp.task('eslint', function () {
 });
 
 gulp.task('lint', ['jscs', 'eslint']);
+
+// Test
+gulp.task('mocha', function () {
+    return gulp.src(paths.test, {read: false})
+        .pipe(mocha());
+});
+
+gulp.task('istanbul', function (done) {
+    gulp.src(paths.coverage)
+        .pipe(istanbul())
+        .pipe(istanbul.hookRequire())
+        .on('finish', function () {
+            gulp.src(paths.test)
+                .pipe(mocha())
+                .pipe(istanbul.writeReports())
+                .on('end', done);
+        });
+});
+
+gulp.task('test', ['lint', 'istanbul']);
